f4946fc0ae8d3d44f3cd8d81cc1ea424a0c064aa
mriveras <mriveras@gmail.com>
2014-07-27 14:27:17 +1000

    sending email
    Try 1

--- php/sendemail.php
+++ php/sendemail.php
@@ -1,17 +1,49 @@
+<html>
+<body>
 <?php
-
-  //Email information
-  $admin_email = "mriveras@gmail.com";
-  // $email = $_REQUEST['email'];
-  // $message = $_REQUEST['name'] || " : " || $_REQUEST['message'];
-  
-  $email = "c20024070@gmail.com";
-  $message = "PRUEBA";
-  $subject = "CONTACT FROM WEBSITE";
-  //send email
-  mail($admin_email, "$subject", $message, "From:" . $email);
-  
-  //Email response
-  echo "Thank you for contacting me!";
- 
+function spamcheck($field) {
+  // Sanitize e-mail address
+  $field=filter_var($field, FILTER_SANITIZE_EMAIL);
+  // Validate e-mail address
+  if(filter_var($field, FILTER_VALIDATE_EMAIL)) {
+    return TRUE;
+  } else {
+    return FALSE;
+  }
+}
 ?>
+
+<h2>Feedback Form</h2>
+<?php
+// display form if user has not clicked submit
+if (!isset($_POST["submit"])) {
+  ?>
+  <form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
+  From: <input type="text" name="from"><br>
+  Subject: <input type="text" name="subject"><br>
+  Message: <textarea rows="10" cols="40" name="message"></textarea><br>
+  <input type="submit" name="submit" value="Submit Feedback">
+  </form>
+  <?php 
+} else {  // the user has submitted the form
+  // Check if the "from" input field is filled out
+  if (isset($_POST["from"])) {
+    // Check if "from" email address is valid
+    $mailcheck = spamcheck($_POST["from"]);
+    if ($mailcheck==FALSE) {
+      echo "Invalid input";
+    } else {
+      $from = $_POST["from"]; // sender
+      $subject = $_POST["subject"];
+      $message = $_POST["message"];
+      // message lines should not exceed 70 characters (PHP rule), so wrap it
+      $message = wordwrap($message, 70);
+      // send mail
+      mail("mriveras@gmail.com",$subject,$message,"From: $from\n");
+      echo "Thank you for sending us feedback";
+    }
+  }
+}
+?>
+</body>
+</html>

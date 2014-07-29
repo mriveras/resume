b784bdadd3408632f1f6305069c690f360e24334
mriveras <mriveras@gmail.com>
2014-07-29 12:18:54 +1000

    sending email
    try 8

--- php/sendemail.php
+++ php/sendemail.php
@@ -1,31 +1,10 @@
 <?php
-require_once "Mail.php";
-
-$from = "c20024070@hotmail.com";
-$to = "mriveras@gmail.com";
-$subject = "Hi!";
-$body = "Hi,\n\nHow are you?";
-
-$host = "smtp.gmail.com";
-$port = "587";
-$username = "mriveras@gmail.com";
-$password = "nhzrdjcvslqdthvn";
-
-$headers = array ('From' => $from,
-  'To' => $to,
-  'Subject' => $subject);
-$smtp = Mail::factory('smtp',
-  array ('host' => $host,
-    'port' => $port,
-    'auth' => true,
-    'username' => $username,
-    'password' => $password));
-
-$mail = $smtp->send($to, $headers, $body);
-
-if (PEAR::isError($mail)) {
-  echo("<p>" . $mail->getMessage() . "</p>");
- } else {
-  echo("<p>Message successfully sent!</p>");
- }
+$sendgrid = new SendGrid('azure_f3f37998d2f5a64574cdaeb953fc8c5f@azure.com', '13iBnTLjSIQ3X1F');
+$mail = new SendGridMail();
+$mail->addTo('mriveras@gmail.com')->
+       setFrom('c20024070@hotmail.com')->
+       setSubject('Subject goes here')->
+       setText('Hello World!')->
+       setHtml('<strong>Hello World!</strong>');
+$sendgrid->smtp->send($mail);
 ?>

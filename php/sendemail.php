1b91fe36c285baa801f6ac83ca4fcce2f999b262
mriveras <mriveras@gmail.com>
2014-07-29 12:36:19 +1000

    sending email
    try 11

--- php/sendemail.php
+++ php/sendemail.php
@@ -1,14 +1,4 @@
-<?php
-$sendgrid = new SendGrid('azure_f3f37998d2f5a64574cdaeb953fc8c5f@azure.com', '13iBnTLjSIQ3X1F');
-$mail = new SendGridMail();
-$mail->addTo('mriveras@gmail.com')->
-       setFrom('c20024070@hotmail.com')->
-       setSubject('Subject goes here')->
-       setText('Hello World!')->
-       setHtml('<strong>Hello World!</strong>');
-$sendgrid->smtp->send($mail);
-?>
 <html>
 <head></head>
-<body>hola prueba 10</body>
+<body>hola prueba 11</body>
 </html>
